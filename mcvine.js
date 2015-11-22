089932c274e2012b6d619fe75c38c01c3e6fbbe5
Jiao Lin <jiao.lin@gmail.com>
2015-11-21 19:15:08 -0500

    mcvine.js: fixed a bug -- do mathjax config after upload successfully

--- mcvine.js
+++ mcvine.js
@@ -1,20 +1,23 @@
 
 (function(mcvine, $, undefined) {
 
-    $.getScript("https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML");
-    $.getScript("jquery-ui/jquery-ui.js");
-    $.getScript("flot/jquery.flot.js");
-    
     mcvine.init = function() {
 	
 	$(function(){
+	    $.getScript(
+		"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML",
+		function () {
+		    MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});
+		}
+	    );
+	    $.getScript("jquery-ui/jquery-ui.js");
+	    $.getScript("flot/jquery.flot.js");
+    
 	    $('<div id="header" class="top">').insertBefore("#page");
 	    $("#header").load("header.html");
 	    $('<div id="footer" class="top">').insertAfter("#page");
 	    $("#footer").load("footer.html");
 	    
-	    // this line has to be the last
-	    MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});
 	});
 
     };
